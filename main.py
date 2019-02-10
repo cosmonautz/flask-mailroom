99f8527485609a5999959249d17fa8c4986294b6
Kevin Waldron <kevinmwaldron@gmail.com>
2019-02-09 16:50:19 -0800

    add new features

--- main.py
+++ main.py
@@ -19,14 +19,33 @@ def all():
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        query = Donor.select().where(Donor.name == request.form['name'])
 
-        if donor:
+        if query.exists():
+            donor = query.get()
+            donation = Donation(donor=donor, value=request.form['donation'])
+            donation.save()
+        else:
+            donor = Donor(name=request.form['name'])
+            donor.save()
             donation = Donation(donor=donor, value=request.form['donation'])
             donation.save()
         return redirect(url_for('all'))
     else:
         return render_template('create.jinja2')
+
+@app.route('/donor/', methods=['GET', 'POST'])
+def view_single():
+    error = None
+    if request.method == 'POST':
+        query = Donor.select().where(Donor.name == request.form['name'])
+        if query.exists():
+            donor = query.get()
+            donations = Donation.select().where(Donation.donor == donor)
+            return render_template('view_single.jinja2', donations=donations, donor_name=donor.name)
+        else:
+            error = f"No donor named {request.form['name']} found"
+    return render_template('view_single.jinja2', error=error)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
